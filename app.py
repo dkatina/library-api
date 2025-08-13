bcd76d31174cb53ba38633c9e849158f1f81f6df
dkatina <108095842+dkatina@users.noreply.github.com>
2025-08-13 19:10:36 -0400

    tables complete

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from datetime import date
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Date, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import Date, ForeignKey, String, Table, Column
 
 app = Flask(__name__) #Instatiating our Flask app
 
@@ -23,6 +23,13 @@ db = SQLAlchemy(model_class = Base)
 
 db.init_app(app) #adding the db to the app.
 
+loan_books = Table(
+    'loan_books',
+    Base.metadata,
+    Column('loan_id', ForeignKey('loans.id')),
+    Column('book_id', ForeignKey('books.id'))
+)
+
 
 class Users(Base):
     __tablename__ = 'users' #lowercase plural form of resource
@@ -34,6 +41,41 @@ class Users(Base):
     DOB: Mapped[date] = mapped_column(Date, nullable=True)
     address: Mapped[str] = mapped_column(String(500), nullable=True)
     role: Mapped[str] = mapped_column(String(30), nullable=False)
+
+    #One to Many relationship from User to Books
+    loans: Mapped[list['Loans']] = relationship('Loans', back_populates='user')
+
+  
+class Loans(Base):
+    __tablename__ = 'loans'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'), nullable=False)
+    loan_date: Mapped[date] = mapped_column(Date, nullable=True)
+    deadline: Mapped[date] = mapped_column(Date, nullable=True)
+    return_date: Mapped[date] = mapped_column(Date, nullable=True)
+
+    #Relationships
+    user: Mapped['Users'] = relationship('Users', back_populates='loans')
+    books: Mapped[list['Books']] = relationship("Books", secondary=loan_books, back_populates='loans') #Many to Many relationship going through the loan_books table
+   
+
+class Books(Base):
+    __tablename__ = 'books'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    genre: Mapped[str] = mapped_column(String(360), unique=True, nullable=False)
+    age_category: Mapped[str] = mapped_column(String(120), nullable=False)
+    publish_date: Mapped[date] = mapped_column(Date, nullable=True)
+    author: Mapped[str] = mapped_column(String(500), nullable=True)
+
+    #Relationship
+    loans: Mapped[list['Loans']] = relationship('Loans', secondary=loan_books, back_populates='loans')
+
+
+
+
 
 
 
