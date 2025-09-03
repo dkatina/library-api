dfe11dc35db3b2fc08cddfa2dd1c5ff9d83296ae
dkatina <108095842+dkatina@users.noreply.github.com>
2025-09-02 21:01:14 -0400

    FIXED?

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,7 @@
 from app.models import db
 from app import create_app
+import os
+
 
 
 app = create_app('ProductionConfig') #Make sure you are in production mode
@@ -8,5 +10,7 @@ app = create_app('ProductionConfig') #Make sure you are in production mode
 with app.app_context():
     # db.drop_all() 
     db.create_all() #Creating our database tables
+
+app.run()
 
 
